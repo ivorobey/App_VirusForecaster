da82364cf58dd4dfc7288a8b1662e9eec48eb342
Roman <42977616+ivorobey@users.noreply.github.com>
2022-05-29 14:44:10 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dash App for Covid-19 Forecast
+
+https://app-virus-forecaster.herokuapp.com/
---
+++ settings/config.py
@@ -0,0 +1,30 @@
+
+import os
+
+
+
+## App settings
+name = "Virus Forecaster"
+
+host = "0.0.0.0"
+
+port = int(os.environ.get("PORT", 5000))
+
+debug = False
+
+contacts = "https://www.linkedin.com/in/mauro-di-pietro-56a1366b/"
+
+code = "https://github.com/mdipietro09/App_VirusForecaster"
+
+tutorial = "https://towardsdatascience.com/how-to-embed-bootstrap-css-js-in-your-python-dash-app-8d95fc9e599e"
+
+fontawesome = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
+
+
+
+## File system
+root = os.path.dirname(os.path.dirname(__file__)) + "/"
+
+
+
+## DB
